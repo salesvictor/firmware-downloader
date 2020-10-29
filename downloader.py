9c5669ae56d0b52596a53a9c0cc1424f207f0dbb
Victor Sales <victor@vsales.io>
2020-10-28 22:38:28 -0300

    Updating to create folders and show progress

--- downloader.py
+++ downloader.py
@@ -14,8 +14,9 @@ soup_html = parsed_url(url)
 routers_table = soup_html.find_all(attrs={'data-class':'wi-fi-routers'})[0]
 routers_tags = routers_table.find_all('a')
 routers = [{'link':_['href'], 'name':_.text} for _ in routers_tags]
-for router in routers:
-    print(f"Downloading firmare for router {router['name']}\n")
+for idx, router in enumerate(routers):
+    router_name = router['name']
+    print(f"Downloading firmare for router {router_name} ({idx+1}/{len(routers)})\n")
     router_url = f"https://www.tp-link.com{router['link']}#Firmware"
     soup_html = parsed_url(router_url)
     tables = soup_html.find_all('table')
@@ -23,7 +24,8 @@ for router in routers:
     downloads = [_ for _ in downloads if _]
     files = [_[0]['href'] for _ in downloads]
     print(f'Found {len(files)} files\n')
+    os.makedirs(router_name, exist_ok=True)
     for idx, file in enumerate(files):
         print(f'Downloading file {idx+1}/{len(files)}')
         link = file.replace(' ', '%20')
-        urllib.request.urlretrieve(link, os.path.basename(file))
+        urllib.request.urlretrieve(link, f'{router_name}/{os.path.basename(file)}')
